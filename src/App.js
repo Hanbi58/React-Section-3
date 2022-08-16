39a906a761c305954b61043d2de460bc315c51f1
Hanbi58 <hanxiaonan2012@gmail.com>
2022-08-16 11:24:55 -0700

    fixed js file issue

--- src/App.js
+++ src/App.js
@@ -5,25 +5,25 @@ const expenses = [
     id: "e1",
     title: "Toilet Paper",
     amount: 94.12,
-    date: new Date(2020, 7, 14).toDateString(),
+    date: new Date(2020, 7, 14),
   },
   {
     id: "e2",
     title: "New TV",
     amount: 799.49,
-    date: new Date(2021, 2, 12).toDateString(),
+    date: new Date(2021, 2, 12),
   },
   {
     id: "e3",
     title: "Car Insurance",
     amount: 294.67,
-    date: new Date(2021, 2, 28).toDateString(),
+    date: new Date(2021, 2, 28),
   },
   {
     id: "e4",
     title: "New Desk (Wooden)",
     amount: 450,
-    date: new Date(2021, 5, 12).toDateString(),
+    date: new Date(2021, 5, 12),
   },
 ];
 
@@ -36,6 +36,7 @@ function App() {
         date={expenses[0].date}
         amount={expenses[0].amount}
       />
+
       <ExpenseItem
         title={expenses[1].title}
         date={expenses[1].date}
@@ -56,3 +57,11 @@ function App() {
 }
 
 export default App;
+
+{
+  /* <h2>Lets Get Started</h2>
+<ExpenseItem props={expenses[0]} />
+<ExpenseItem props={expenses[1]} />
+<ExpenseItem props={expenses[2]} />
+<ExpenseItem props={expenses[3]} /> */
+}
