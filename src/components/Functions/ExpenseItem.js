43a5db7714adb736cc29f6cc834cc7eb756d80b4
Hanbi58 <hanxiaonan2012@gmail.com>
2022-08-19 17:29:17 -0700

    intro to state

--- src/components/Functions/ExpenseItem.js
+++ src/components/Functions/ExpenseItem.js
@@ -1,14 +1,23 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 
+import { useState } from "react";
+
 function ExpenseItem(props) {
+  const [ctt, setCtt] = useState(props.item.title);
+
+  const changeTitle = () => {
+    setCtt("Updated");
+  };
+
   return (
     <div className="expense-item">
       <ExpenseDate date={props.item.date} />
       <div className="expense-item__description">
-        <h2>{props.item.title}</h2>
+        <h2>{ctt}</h2>
       </div>
       <div className="expense-item__price">${props.item.amount}</div>
+      <button onClick={changeTitle}>Click</button>
     </div>
   );
 }
